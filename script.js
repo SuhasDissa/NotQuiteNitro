7410bda984ea65878c9edc5758d6e899b0f31e88
Denis Çerri <64997243+deniscerri@users.noreply.github.com>
2022-12-03 11:04:31 +0100

    added sticker support
    animated stickers will just show as a single frame

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 $(document).ready(function () {
-    //setInterval(function () { $("img").attr("draggable", "true"); }, 1000);
+    setInterval(function () { $("img").attr("draggable", "true"); }, 1000);
+    //emotes
     setInterval(function () {
         $("img").css('pointer-events','all');
         $("div[class*='listItems'] div[class*='categorySection'] ul li button").attr("style", "filter: grayscale(0);");
         $("div[class*='premiumPromo']").remove();
     }, 100);
+    //stickers
+    setInterval(function () {
+        $("img").css('pointer-events','all');
+        $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode']").attr("style", "filter: grayscale(0);");
+        $("div[class*='upsellWrapper']").remove();
+    }, 100);
+    //emote-onclick
     setInterval(function () {
         $("div[class*='listItems'] div[class*='categorySection'] ul li button img[class*='image']").each(function () {
-            if ($(this).attr("fuckedup") != "true") {
-                $(this).attr("fuckedup", "true");
+            if ($(this).attr("affected") != "true") {
+                $(this).attr("affected", "true");
                 $(this).click(function (e) {
                     let ufsource = e.currentTarget.getAttribute('src');
                     const url = ufsource.split("?size=");
-                    source = url[0] + "?size=64"
+                    source = url[0] + "?size=48"
+                    var $temp = jQuery("<input>");
+                    jQuery("body").append($temp);
+                    $temp.val(source).select();
+                    document.execCommand("copy");
+                    $temp.remove();
+                });
+            }
+        });
+    }, 1000);
+    //sticker-onclick
+    setInterval(function () {
+        $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode'] div img[class*='Image']").each(function () {
+            if ($(this).attr("affected") != "true") {
+                $(this).attr("affected", "true");
+                $(this).click(function (e) {
+                    let ufsource = e.currentTarget.getAttribute('src');
+                    const url = ufsource.split("?size=");
+                    source = url[0] + "?size=256"
                     var $temp = jQuery("<input>");
                     jQuery("body").append($temp);
                     $temp.val(source).select();
