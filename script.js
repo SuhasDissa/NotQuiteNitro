e958e82358c0f0c2a77f6e1b1457fd3e35ca193a
Suhas Dissanayake <64766434+SuhasDissa@users.noreply.github.com>
2022-12-04 20:41:17 +0530

    Performance improvements

--- script.js
+++ script.js
@@ -1,50 +1,36 @@
 $(document).ready(function () {
-    setInterval(function () { $("img").attr("draggable", "true"); }, 1000);
+    function clickToCopy(e,size){
+                    let ufsource = e.currentTarget.getAttribute('src');
+                    const url = ufsource.split("?size=");
+                    source = url[0] + "?size=" + size
+                    var $temp = jQuery("<input>");
+                    jQuery("body").append($temp);
+                    $temp.val(source).select();
+                    document.execCommand("copy");
+                    $temp.remove();
+                }
     //emotes
     setInterval(function () {
         $("img").css('pointer-events','all');
-        $("div[class*='listItems'] div[class*='categorySection'] ul li button").attr("style", "filter: grayscale(0);");
+        $("div[class*='listItems'] div[class*='categorySection'] ul li button").css('filter',' grayscale(0)');
         $("div[class*='premiumPromo']").remove();
-    }, 100);
     //stickers
-    setInterval(function () {
-        $("img").css('pointer-events','all');
-        $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode']").attr("style", "filter: grayscale(0);");
+        $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode']").css('filter',' grayscale(0)');
         $("div[class*='upsellWrapper']").remove();
     }, 100);
-    //emote-onclick
     setInterval(function () {
+        //emote-onclick
         $("div[class*='listItems'] div[class*='categorySection'] ul li button img[class*='image']").each(function () {
             if ($(this).attr("affected") != "true") {
                 $(this).attr("affected", "true");
-                $(this).click(function (e) {
-                    let ufsource = e.currentTarget.getAttribute('src');
-                    const url = ufsource.split("?size=");
-                    source = url[0] + "?size=48"
-                    var $temp = jQuery("<input>");
-                    jQuery("body").append($temp);
-                    $temp.val(source).select();
-                    document.execCommand("copy");
-                    $temp.remove();
-                });
+                $(this).click(clickToCopy(e,"48"));
             }
         });
-    }, 1000);
-    //sticker-onclick
-    setInterval(function () {
+        //sticker-onclick
         $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode'] div img[class*='Image']").each(function () {
             if ($(this).attr("affected") != "true") {
                 $(this).attr("affected", "true");
-                $(this).click(function (e) {
-                    let ufsource = e.currentTarget.getAttribute('src');
-                    const url = ufsource.split("?size=");
-                    source = url[0] + "?size=256"
-                    var $temp = jQuery("<input>");
-                    jQuery("body").append($temp);
-                    $temp.val(source).select();
-                    document.execCommand("copy");
-                    $temp.remove();
-                });
+                $(this).click(clickToCopy(e,"256"));
             }
         });
     }, 1000);
