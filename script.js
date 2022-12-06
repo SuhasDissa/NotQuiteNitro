684bb68a7749097f830306bb3f49b0e8724c33b1
Suhas Dissanayake <64766434+SuhasDissa@users.noreply.github.com>
2022-12-06 10:08:31 +0530

    Make code less bloated

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 $(document).ready(function () {
-    //emotes
     setInterval(function () {
-        //$("img").css('pointer-events','all'); // bad for performance
-        $("div[class*='listItems'] div[class*='categorySection'] ul li button").css('filter',' grayscale(0)');
+        // make emotes coloured and clickable
+        $("div[class*='listItems'] div[class*='categorySection'] ul li button").css('filter',' grayscale(0)').children("img").css('pointer-events','all');
+        // make stickers coloured and clickable
+        $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode']").css('filter',' grayscale(0)').children("div img").css('pointer-events','all');
+
+        //Remove annoying nitro banner that appears when emote is clicked
         $("div[class*='premiumPromo']").remove();
-    //stickers
-        $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode']").css('filter',' grayscale(0)');
         $("div[class*='upsellWrapper']").remove();
     }, 100);
     setInterval(function () {
-        //emote-onclick
+        // Add click event listener to the emotes
         $("div[class*='listItems'] div[class*='categorySection'] ul li button img[class*='image']").each(function () {
             if ($(this).attr("affected") != "true") {
+                // adding "affected" attribute to prevent duplicaton of click event listeners
                 $(this).attr("affected", "true");
                 $(this).click((e)=>{
                     let ufsource = e.currentTarget.getAttribute('src');
                     const url = ufsource.split("?size=");
                     source = url[0] + "?size=48"
+
+                    // A really bad way to copy image link to clipboard
                     var $temp = jQuery("<input>");
                     jQuery("body").append($temp);
                     $temp.val(source).select();
@@ -25,7 +29,6 @@ $(document).ready(function () {
                 });
             }
         });
-        //sticker-onclick
         $("div[class*='listItems'] div div[class*='row'] div[role*='gridcell'] div[class*='sticker'] div div[class*='stickerNode'] div img[class*='Image']").each(function () {
             if ($(this).attr("affected") != "true") {
                 $(this).attr("affected", "true");
